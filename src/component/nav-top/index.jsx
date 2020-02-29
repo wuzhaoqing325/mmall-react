6da0bafa6a22923a7e3e968afc8b2d3663906b52
wuzhaoqing325 <1586607026@qq.com>
2020-02-29 15:08:55 +0800

    base page

--- src/component/nav-top/index.jsx
+++ src/component/nav-top/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import MUtil from 'util/mm.jsx';
+import User from 'service/user-service.jsx';
 
-import {BrowserRouter as Router, Route, Redirect, Link, Switch} from 'react-router-dom';
+const _mm = new MUtil();
+const _user = new User();
 
 class NavTop extends React.Component{
 	constructor(props){
 		super(props);
+        this.state = {
+            username:_mm.getStorage('userInfo').username
+        }
 	}
+    // 退出登录
+    onLogout(){
+        _user.logout().then(res => {
+            _mm.removeStorage('userInfo');
+            window.location.herf = '/login';
+        }, errMsg => {
+            _mm.errorTips(errMsg);
+        })
+    }
+
 	render(){
 		return (
 			<div className="navbar navbar-default top-navbar">
@@ -16,19 +33,18 @@ class NavTop extends React.Component{
                     <li className="dropdown">
                         <a className="dropdown-toggle" href="#" aria-expanded="false">
                             <i className="fa fa-user fa-fw"></i> 
-                            {/*
+
                             {
                                 this.state.username
                                 ? <span>欢迎，{this.state.username}</span>
                                 : <span>欢迎您</span>
                             }
-                            */}
+
                             <i className="fa fa-caret-down"></i>
                         </a>
                         <ul className="dropdown-menu dropdown-user">
                             <li>
-                                {/*<a onClick={()=>this.onLogout()}>*/}
-                                <a>
+                                <a onClick={()=>this.onLogout()}>
                                     <i className="fa fa-sign-out-alt fa-fw"></i>
                                     <span>退出登录</span>
                                 </a>
--- src/util/mm.jsx
+++ src/util/mm.jsx
@@ -1,4 +1,85 @@
 class MUtil{
+	request(Param){
+		return new Promise((resolve, reject) => {
+			
+			$.ajax({
+				type:Param.type || 'get',
+				url:Param.url || '',
+				dataType:Param.dataType || 'json',
+				data:Param.data || null,
+				success : res => {
+					// 数据请求成功
+					
+					if(0 === res.status){
+						typeof resolve === 'function' && resolve(res.data, res.msg)
+					}
+					// 没有登录状态，强制登录
+					else if (10 === res.status){
+						this.doLogin();
+						
+					}
+					else{
+						typeof reject === 'function' && reject(res.msg || res.data)
+					}
+				},
+				error : err => {
+					typeof reject === 'function' && reject(err.statusText)	
+					
+				}
+			});
+		});
+	}
+
+	// 跳转登录
+	doLogin(){
+		window.location.herf = '/login?redirect=' + encodeURIComponent(window.location.pathname)
+	}
+
+	// 获取url参数
+	getUrlParam(name){
+		// xxxx.com?param=123&param1=456
+		let queryString = window.location.search.split('?')[1] || '';
+		let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"),
+		result = queryString.match(reg);
+		// result: ['param=123', '', '123', '&']
+		return result ? decodeURIComponent(result[2]): null;
+	}
+
+	// 错误提示
+	errorTips(errMsg){
+		alert(errMsg || '好像哪里不对了~');
+	}
+	// 设置本地存储
+	setStorage(name, data){
+		let dataType = typeof data;
+		// JSON对象
+		if(dataType === 'object'){
+			window.localStorage.setItem(name, JSON.stringify(data));	
+		}
+		// 基础类型
+		else if(['number', 'string', 'boolean'].indexOf(dataType) >= 0){
+			window.localStorage.setItem(name, data);
+		}
+		// 其他不支持类型
+		else{
+			alert('该类型不能用于本地存储');
+		}
+	}
+	// 取出本地存储内容
+	getStorage(name){
+		let data = window.localStorage.getItem(name);
+		if(data){
+			return JSON.parse(data);
+		}
+		else{
+			return '';
+		}
+	}
+	// 删除本地存储
+	removeStorage(name){
+		window.localStorage.removeItem(name);
+	}
+
 
 }
 export default MUtil;
